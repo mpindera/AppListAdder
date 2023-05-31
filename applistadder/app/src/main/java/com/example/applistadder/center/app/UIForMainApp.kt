b488e915ce51c3ded5795d36c87c5d68c6989edd
mpindera <mikolaj.pindera@op.pl>
2023-05-31 10:57:33 +0200

    Update UI

--- applistadder/app/src/main/java/com/example/applistadder/center/app/UIForMainApp.kt
+++ applistadder/app/src/main/java/com/example/applistadder/center/app/UIForMainApp.kt
@@ -1,15 +1,23 @@
 package com.example.applistadder.center.app
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.applistadder.ui.theme.lightBrown
 import com.example.applistadder.ui.theme.lightGray
@@ -17,25 +25,28 @@ import com.example.applistadder.ui.theme.lightGray
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun UIForMainApp() {
-    Scaffold() {
-        /*TODO make "Have a great day $username"*/
-        TopAppBar(
-            title = {
-                Text(color = lightGray, text = "Have a great day ")
-            },
-            actions = {
-                IconButton(onClick = {
-
-                }) {
+    var searchText by remember {
+        mutableStateOf("")
+    }
+    TopAppBar(
+        title = {
+            TextField(
+                value = searchText,
+                onValueChange = { searchText = it },
+                modifier = Modifier.fillMaxWidth(),
+                textStyle = TextStyle(color = Color.White),
+                placeholder = { Text(color = lightGray, text = "Have a great day ") },
+                leadingIcon = {
                     Icon(Icons.Default.Search, "Search")
                 }
-            },
-            backgroundColor = lightBrown
-        )
-    }
+            )
+        },
+        backgroundColor = lightBrown
+    )
 
 }
 
+//Text(color = lightGray, text = "Have a great day ")
 @Preview(showBackground = true)
 @Composable
 fun UIForMainAppPreview() {
